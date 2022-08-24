9918f92b63167798c24801efe61556d971c746b4
ERLMKea <erlm@kea.dk>
2022-08-24 10:49:11 +0200

    class getclass javascript

--- src/Main.java
+++ src/Main.java
@@ -32,26 +32,29 @@ public class Main {
     }
 
     public static <T> void out(List<T> lst) {
+        System.out.println("I am List size=" + lst.size());
         System.out.println(lst);
     }
 
-    public static void out(Object std) {
+    public static void out23(Object std) {
+
         System.out.println(std);
     }
 
+    public static void out(Class cls) {
+        System.out.println("I am class name =" + cls.getName());
+        System.out.println(cls);
+    }
+
     public static void main(String[] args) {
-        out('a');
-        out('b');
-        out('1');
-        char c = (char)97;
-        out('ö');
-        out("" + c);
+        Student std = new Student();
+        std.setAge(23);
+        std.setName("kurt");
+        Class cls = std.getClass();
+        out(cls);
+        var supcls = cls.getClass();
+        out(supcls);
 
-        out('球');
-
-        for (int i = 29690; i<29800; i++) {
-            out("" + (char)i + " ascii=" + i);
-        }
 
     }
 
